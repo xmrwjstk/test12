0e6b1f2858792cd4111cc25bd9d5cee9a9658b21
xmrwjstk <152825145+xmrwjstk@users.noreply.github.com>
2023-12-11 11:10:21 +0900

    Add files via upload

--- tt12.py
+++ tt12.py
@@ -4,9 +4,9 @@ import streamlit as st
 fig,ax = plt.subplots()
 
 
-c1 = st.sideber.radio('서느이 색을 선택하시오',['red', 'green', 'blue', 'orange'])
-s1 = st.sideber.radio('선의 스타일을 선택하시오'['-', ':', '-.', '--'])
-m1 = st.sideber.radio('마커의 스타일을 선택하시오'['p','s','p','h'])
+c1 = st.sidebar.radio('선의 색을 선택하시오',['red', 'green', 'blue', 'orange','purple'])
+s1 = st.sidebar.radio('선의 스타일을 선택하시오'['-', ':', '-.', '--'])
+m1 = st.sidebar.radio('마커의 스타일을 선택하시오'['p','s','p','h'])
 
 love = []
 y = []
@@ -17,3 +17,6 @@ for i in range(-20, 21, 3):
 plt.plot(love, y, color = c1, linestyle = s1, maker = m1)
 
 st.pyplot(fig)
+
+
+
